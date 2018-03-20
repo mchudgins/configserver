a880026b043f1c4ec162f754cadf6fcfeee80e60
Mike Hudgins <mchudgins@gmail.com>
2018-03-19 20:20:56 -0500

    WIP

--- src/main/docker/docker-entrypoint.sh
+++ src/main/docker/docker-entrypoint.sh
@@ -1,7 +1,56 @@
 #!/bin/bash
 #set -e
 
+VAULT_TOKEN_FILE=/usr/local/etc/vault/token
+KEYSTORE=/usr/local/etc/keystore.p12
+
 if [[ "$1" = 'runapp' ]]; then
+
+    echo Environment
+    echo "-----------"
+    printenv | sort
+    echo "-----------"
+    echo "vault version: " `vault version`
+
+    if [[ -z "${POD_NAMESPACE}" ]]; then
+        echo "Use the 'downward api' to define the pod's namespace as POD_NAMESPACE! Exiting..."
+        exit 1
+    fi
+
+    # we need a vault token to bootstrap our various secrets
+    if [[ -z "${VAULT_TOKEN}" ]]; then
+        if [[ ! -f ${VAULT_TOKEN_FILE} ]]; then
+            echo "Unable to read Vault token. ${VAULT_TOKEN_FILE} is missing. Exiting..."
+            exit 1
+        fi
+        VAULT_TOKEN=$(cat ${VAULT_TOKEN_FILE})
+    fi
+
+    GIT_REPO_PWORD=$(vault read -address=${VAULT_ADDR} -field=password secret/projects/${POD_NAMESPACE}/gitlab)
+    JKS_KEY=$(vault read -addres=${VAULT_ADDR} -field=key secret/projects/${POD_NAMESPACE}/jks}
+
+    #
+    # check if the jks file was previously created, if not construct it
+    #
+    if [[ -f ${KEYSTORE} ]]; then
+        echo "${KEYSTORE} missing.  Will generate it now."
+
+        tmpdir=`mktemp -d`
+        cd ${tmpdir}
+        git config --global credential.helper cache --timeout 120
+        git config --global credential.https://gitlab.com.dstcorp ${GIT_REPO_PWORD}
+        git clone ${GIT_REPO_URL}
+        clonedir=`basename ${GIT_REPO_URL} .git`
+
+        if [[ ! -f ${clonedir}/certificates/config.dst.cloud.pem ]]; then
+            echo "Unable to locate public key file certificates/config.dst.cloud.pem! Exiting..."
+            exit 1
+        fi
+
+        
+        cd -
+
+    fi
 
 	#
 	# create the client cert file from the cert + key
