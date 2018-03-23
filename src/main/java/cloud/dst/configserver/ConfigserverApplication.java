7ef4d501e50d9c98e26cb6742832bd54c121dcbe
Mike Hudgins <mchudgins@gmail.com>
2018-03-23 09:06:39 -0500

    Add annotations to enable config server

--- src/main/java/cloud/dst/configserver/ConfigserverApplication.java
+++ src/main/java/cloud/dst/configserver/ConfigserverApplication.java
@@ -1,9 +1,13 @@
 package cloud.dst.configserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EnableConfigServer
 public class ConfigserverApplication {
 
 	public static void main(String[] args) {
